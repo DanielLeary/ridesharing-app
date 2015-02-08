e6938b21649d5a579226ecee8a388f0390a9b6e0
Vaneet Mehta <vaneetmehta.14@imperial.ac.uk>
2015-02-08 01:21:34 +0000

    Simple Map with location

---
+++ RideSharingMap/RideSharingMap/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  RideSharingMap
+//
+//  Created by Vaneet Mehta on 07/02/2015.
+//  Copyright (c) 2015 Vaneet Mehta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface ViewController : UIViewController <MKMapViewDelegate>
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+
+
+@end
+
---
+++ RideSharingMap/RideSharingMap/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  RideSharingMap
+//
+//  Created by Vaneet Mehta on 07/02/2015.
+//  Copyright (c) 2015 Vaneet Mehta. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <CLLocationManagerDelegate>
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+        [self.locationManager requestWhenInUseAuthorization];
+    }
+    [self.locationManager startUpdatingLocation];
+    
+    _mapView.showsUserLocation = YES;
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    NSLog(@"%@", [locations lastObject]);
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
