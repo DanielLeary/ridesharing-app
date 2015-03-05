11c841d835dcbcb2c59cdb278ce115e252c4e0f3
Han, Lin <lin.han14@imperial.ac.uk>
2015-03-05 14:50:19 +0000

    Created MapViewModelClass

---
+++ RideSharingMap/RideSharingMap/MapViewModel.h
@@ -0,0 +1,13 @@
+//
+//  MapViewModel.h
+//  RideSharingMap
+//
+//  Created by Han, Lin on 05/03/2015.
+//  Copyright (c) 2015 Vaneet Mehta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapViewModel : NSObject
+
+@end
---
+++ RideSharingMap/RideSharingMap/MapViewModel.m
@@ -0,0 +1,13 @@
+//
+//  MapViewModel.m
+//  RideSharingMap
+//
+//  Created by Han, Lin on 05/03/2015.
+//  Copyright (c) 2015 Vaneet Mehta. All rights reserved.
+//
+
+#import "MapViewModel.h"
+
+@implementation MapViewModel
+
+@end
