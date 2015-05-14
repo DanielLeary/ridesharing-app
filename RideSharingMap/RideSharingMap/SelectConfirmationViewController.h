0f7c7f889d9e16a15b805e5d7d35cd28760e4909
Shah, Priyav <priyav.shah14@imperial.ac.uk>
2015-05-14 16:53:31 +0100

    branch incase merge messes up

---
+++ RideSharingMap/RideSharingMap/SelectConfirmationViewController.h
@@ -0,0 +1,24 @@
+//
+//  SelectConfirmationViewController.h
+//  RideSharingMap
+//
+//  Created by Shah, Priyav on 14/05/2015.
+//  Copyright (c) 2015 Vaneet Mehta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import "Ride.h"
+@interface SelectConfirmationViewController : UIViewController <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
+@property (weak, nonatomic) IBOutlet UILabel *driverName;
+@property (weak, nonatomic) IBOutlet UILabel *date;
+@property (weak, nonatomic) IBOutlet UILabel *time;
+@property (weak, nonatomic) IBOutlet UILabel *pickupLocation;
+@property (weak, nonatomic) IBOutlet UILabel *destinationLocation;
+@property (weak, nonatomic) IBOutlet MKMapView *map;
+- (IBAction)submitRequest:(UIButton *)sender;
+@property Ride* ride;
+
+
+@end
---
+++ RideSharingMap/RideSharingMap/SelectConfirmationViewController.m
@@ -0,0 +1,71 @@
+//
+//  SelectConfirmationViewController.m
+//  RideSharingMap
+//
+//  Created by Shah, Priyav on 14/05/2015.
+//  Copyright (c) 2015 Vaneet Mehta. All rights reserved.
+//
+
+#import "SelectConfirmationViewController.h"
+
+@interface SelectConfirmationViewController ()
+
+@end
+
+@implementation SelectConfirmationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    PFUser *driver = self.ride.drivers[self.ride.rowNumber];
+    PFObject* offer = self.ride.rideOffers[self.ride.rowNumber];
+    
+    NSString *driverName = driver[@"Name"];
+    NSLog(@"%@", driverName);
+    NSDate* date = offer[@"dateTimeStart"];
+    
+    // date formatting stuff
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"EEE, d MMM"];
+    NSString* dateString = [dateFormatter stringFromDate:date];
+    
+    // time formatting stuff
+    NSDateFormatter *timeFormatter = [[NSDateFormatter alloc] init];
+    [timeFormatter setDateFormat:@"HH:MM"];
+    NSString* timeString = [timeFormatter stringFromDate:date];
+    
+    
+    self.driverName.text = driverName;
+    self.date.text = dateString;
+    self.time.text = timeString;
+    
+    PFFile *userImageFile = driver[@"ProfilePicture"];
+    [userImageFile getDataInBackgroundWithBlock:^(NSData *imageData, NSError *error) {
+        if (!error) {
+            self.profileImage.image = [UIImage imageWithData:imageData];
+        }
+    }];
+
+    
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)submitRequest:(UIButton *)sender {
+}
+@end
